1355430d55d4cbf64363a004d0ccb72f53735643
Sebin <sebin.mathew@adbrew.io>
2025-03-17 02:47:32 +0530

    move index building during server startup

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"github.com/sebzz2k2/vaultic/logger"
 	"github.com/sebzz2k2/vaultic/server"
+	"github.com/sebzz2k2/vaultic/utils"
 )
 
 func main() {
 	logger.Infof("Starting Vaultic server")
+	logger.Infof("Building index")
+	b := server.NewIndexBuilder(utils.FILENAME, utils.DELIMITER[0])
+	err := b.BuildIndexes()
+	if err != nil {
+		logger.Errorf("Error building index %s", err.Error())
+		return
+	}
+	logger.Infof("Finished building index")
 	server.Start(":5381")
 }
