b9e94dc7bd4613d39acb41bb79d8a98c55e987c3
Sebin <sebinsebzz2002@gmail.com>
2025-04-03 02:25:08 +0530

    feat: implement encoding and decoding functions for Vaultic storage

---
+++ kv_store/storage/encode.go
@@ -0,0 +1,69 @@
+package storage
+
+import "github.com/sebzz2k2/vaultic/utils"
+
+/*
+0th bit deleted 0 or 1
+1st bit version 0 or 1
+3rd - 7th bit reserved 0 for now
+*/
+func EncodeFlags(flags ...bool) byte {
+	var encoded byte
+	for i, flag := range flags {
+		if flag {
+			encoded |= 1 << i
+		}
+	}
+	return encoded
+}
+
+/*
+4 bytes length
+1 byte version
+1 byte flags
+4 bytes key+value CRC
+8 bytes timestamp
+2 bytes key length
+4 bytes value length
+<key length> bytes key
+<value length> bytes value
+*/
+func EncodeData(version int, deleted bool, ts uint64, key, value string) []byte {
+	keyLen := uint16(len(key))     // 2 bytes for key length
+	valueLen := uint32(len(value)) // 4 bytes for value length
+
+	// Calculate total length (including the 4-byte length field)
+	totalLength := 4 + 1 + 1 + 4 + 8 + 2 + 4 + len(key) + len(value) // 4 bytes for length field
+
+	encoded := make([]byte, 0, totalLength) // Preallocate memory
+
+	// Store total length (first 4 bytes)
+	encoded = append(encoded,
+		byte(totalLength>>24), byte(totalLength>>16), byte(totalLength>>8), byte(totalLength))
+
+	flags := EncodeFlags(deleted, false)
+	encoded = append(encoded, byte(version))
+	encoded = append(encoded, flags)
+
+	// Store keyValCRC (4 bytes)
+	keyValCRC := utils.Crc32(key + value)
+	encoded = append(encoded, byte(keyValCRC>>24), byte(keyValCRC>>16), byte(keyValCRC>>8), byte(keyValCRC))
+
+	// Store timestamp (8 bytes)
+	encoded = append(encoded,
+		byte(ts>>56), byte(ts>>48), byte(ts>>40), byte(ts>>32),
+		byte(ts>>24), byte(ts>>16), byte(ts>>8), byte(ts))
+
+	// Store key length (2 bytes)
+	encoded = append(encoded, byte(keyLen>>8), byte(keyLen))
+
+	// Store value length (4 bytes)
+	encoded = append(encoded,
+		byte(valueLen>>24), byte(valueLen>>16), byte(valueLen>>8), byte(valueLen))
+
+	// Append key and value
+	encoded = append(encoded, key...)
+	encoded = append(encoded, value...)
+
+	return encoded
+}
--- utils/utils.go
+++ utils/utils.go
@@ -11,3 +11,20 @@ func WriteToClient(client io.Writer, message string) {
 func Tokenize(inp []byte) []string {
 	return strings.Fields(string(inp))
 }
+
+func Crc32(data string) uint32 {
+	var crc uint32 = 0xFFFFFFFF
+	poly := uint32(0x04C11DB7)
+
+	for _, b := range []byte(data) {
+		crc ^= uint32(b) << 24
+		for i := 0; i < 8; i++ {
+			if (crc & 0x80000000) != 0 {
+				crc = (crc << 1) ^ poly
+			} else {
+				crc <<= 1
+			}
+		}
+	}
+	return crc ^ 0xFFFFFFFF
+}
